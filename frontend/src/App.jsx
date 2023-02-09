20092a26abbc4232019cd97705cca82698d52261
SHARUN266 <sharunattari266@gmail.com>
2023-02-09 12:29:02 +0530

    RandomWordGenerate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import { generate } from './utils/RandomWordGenerator'
+
+function App() {
+ 
+  return (
+    <div >
+      {generate()}
+    </div>
+  )
+}
+
+export default App
